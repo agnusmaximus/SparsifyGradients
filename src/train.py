5511b21fc2334969c16ebdcbb4a20715e29092b5
Max <agnusmaximus@berkeley.edu>
2017-04-15 19:32:36 -0700

    Update

--- src/train.py
+++ src/train.py
@@ -27,7 +27,8 @@ def aggregate_and_apply_gradients(sess, variables, com, rank, n_workers, materia
     if FLAGS.sparsify and rank != 0:
         percentile_cutoff = .90
         thresholds = [np.percentile(x, percentile_cutoff) for x in materialized_grads]
-        sparsified = [x * (x > threshold) for x, threshold in zip(materialized_grads, thresholds)]
+        #sparsified = [x * (x > threshold) for x, threshold in zip(materialized_grads, thresholds)]
+        sparsified = [np.zeros(shape=x.shape) for x, threshold in zip(materialized_grads, thresholds)]
 
         for i, sparse_grad in enumerate(sparsified):
             non_compressed_array = io.BytesIO()
