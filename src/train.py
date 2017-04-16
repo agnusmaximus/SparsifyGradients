b7b39e77d26e2ce4b33f3632857e20dacc97e26e
Max <agnusmaximus@berkeley.edu>
2017-04-15 20:19:15 -0700

    Update

--- src/train.py
+++ src/train.py
@@ -36,9 +36,9 @@ def aggregate_and_apply_gradients(sess, variables, com, rank, n_workers, materia
     if rank == 0:
         for worker in range(1, n_workers):
             print("Master applying gradients for worker %d" % worker)
-            #if FLAGS.sparsify:
+            if FLAGS.sparsify:
                 # Decode sparse matrix
-                #all_gradients = [x.todense().reshape(variables[i].get_shape()) for x in all_gradients]
+                all_gradients = [x.todense() for x in all_gradients]
             #fd = {apply_gradients_placeholder[i] : all_gradients[worker][i] for i in range(len(apply_gradients_placeholder))}
             #sess.run(apply_gradients_op, feed_dict=fd)
 
