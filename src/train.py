dd31411aead0bc2b3c95f851ee21c1b6f70c762a
Max <agnusmaximus@berkeley.edu>
2017-04-15 19:50:38 -0700

    Update

--- src/train.py
+++ src/train.py
@@ -29,7 +29,9 @@ def aggregate_and_apply_gradients(sess, variables, com, rank, n_workers, materia
         thresholds = [np.percentile(x, percentile_cutoff) for x in materialized_grads]
         sparsified = [x * (x > threshold) for x, threshold in zip(materialized_grads, thresholds)]
         sparsified_flatten = [x.flatten() for x in sparsified]
+        print("DENSE: ", sum([x.nbytes for x in materialized_grads]))
         materialized_grads = [sparse.csr_matrix(x) for x in sparsified_flatten]
+        print("SPARSE: ", sum([x.nbytes for x in materialized_grads]))
     all_gradients = com.gather(materialized_grads, root=0)
     if rank == 0:
         for worker in range(1, n_workers):
