b60cd9e5c9626249b521708ae99af6231b863c74
Max <agnusmaximus@berkeley.edu>
2017-04-17 12:07:49 -0700

    Add figures, writeup and plotting code

--- plot/plot_figures.py
+++ plot/plot_figures.py
@@ -41,6 +41,32 @@ def time_to_target_accuracy(data, target):
             return a
     assert(0)
 
+def extract_time_to_aggregate(fname):
+    t = -1
+    f = open(fname, "r")
+    for line in f:
+        if "Mean acc gradients time:" in line:
+            t = float(line.strip().split(":")[-1])
+    f.close()
+    assert(t > 0)
+    return t
+
+def plot_aggregation_time_comparisons(files):
+    plt.cla()
+    time_to_aggregate = []
+    app_names = []
+    for name, fname in files.items():
+        time_to_aggregate.append(extract_time_to_aggregate(fname))
+        app_names.append(name)
+    ind = np.arange(len(time_to_aggregate))
+    width = .8
+    plt.bar(ind, time_to_aggregate, width=width)
+    plt.xticks(ind, app_names)
+    plt.xlabel("Method")
+    plt.title("Time to perform gradient aggregation")
+    plt.ylabel("Time (seconds)")
+    plt.savefig("SparsifyTimeToPerformGradientAggregation.png")
+
 def plot_time_until_accuracy(data, target_acc=.995):
     methods = []
     values = []
@@ -52,7 +78,7 @@ def plot_time_until_accuracy(data, target_acc=.995):
     ind = np.arange(len(values))
     width = .8
     plt.bar(ind, values, width=width)
-    plt.xticks(ind + width / 2, methods)
+    plt.xticks(ind, methods)
     plt.xlabel("Method")
     plt.title("Seconds to reach accuracy %f" % target_acc)
     plt.ylabel("Seconds to reach accuracy %f" % target_acc)
@@ -78,7 +104,7 @@ def plot_time_vs_loss(data):
         plt.plot(times, losses, label=method_name)
     plt.xlabel("Time (seconds)")
     plt.ylabel("Training Loss")
-    plt.legend(loc="upper left")
+    plt.legend(loc="bottom right")
     plt.title("Time vs Loss")
     plt.savefig("SparsifyTimeVsLoss.png")
 
@@ -94,8 +120,35 @@ def plot_time_vs_epoch(data):
     plt.title("Time vs Passes of Data")
     plt.savefig("SparsifyTimeVsEpoch.png")
 
+def plot_epoch_vs_accuracy(data):
+    plt.cla()
+    for method_name, data_values in data.items():
+        epochs = extract_epochs(data_values)
+        accuracies = extract_accuracies(data_values)
+        plt.plot(epochs, accuracies, label=method_name)
+    plt.xlabel("Number of passes of data")
+    plt.ylabel("Training Accuracy")
+    plt.legend(loc="lower right")
+    plt.title("Number of Passes of Data vs Training Accuracy")
+    plt.savefig("EpochVsAccuracy.png")
+
+def plot_epoch_vs_loss(data):
+    plt.cla()
+    for method_name, data_values in data.items():
+        epochs = extract_epochs(data_values)
+        losses = extract_losses(data_values)
+        plt.plot(epochs, losses, label=method_name)
+    plt.xlabel("Number of passes of data")
+    plt.ylabel("Loss")
+    plt.legend(loc="upper right")
+    plt.title("Number of Passes of Data vs Loss")
+    plt.savefig("EpochVsLoss.png")
+
 data_values = {k:extract_values(v) for k,v in files.items()}
 plot_time_until_accuracy(data_values)
 plot_time_vs_loss(data_values)
 plot_time_vs_accuracy(data_values)
 plot_time_vs_epoch(data_values)
+plot_aggregation_time_comparisons(files)
+plot_epoch_vs_accuracy(data_values)
+plot_epoch_vs_loss(data_values)
